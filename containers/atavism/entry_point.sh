e21082076f6eeabb4cc480225780130486003bdb
Ken Barber <ken@bob.sh>
2019-06-02 06:05:37 +0100

    reptyr the process

--- containers/atavism/entry_point.sh
+++ containers/atavism/entry_point.sh
@@ -4,17 +4,19 @@ set -xe
 
 pushd /opt/atavism_server/bin
 
+mkdir -p run/master
+mkdir -p run/world
+
 case "$1" in
     auth)
         CMD="./auth.sh -v"
         $CMD start
-        jobs
+        reptyr -L -T $(<run/master/auth.pid)
         ;;
     *)
         CMD="./world.sh -v"
         jobs
         $CMD $1
+        reptyr -L -T $(<run/world/$1.pid)
         ;;
 esac
-
-tail -f /dev/null
